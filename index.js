5396f2137993a4485881fe5e6eaefe7bbbc34bfe
Ryan Purcell <patrick.ryan.purcell@gmail.com>
2019-01-02 22:17:42 -0600

    Added single route for compare and added error handing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger_base');
 const cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
@@ -14,5 +14,10 @@ app.use(cors());
 app.use('/api', require('./routes/api'));
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(function (err, req, res) {
+    console.error(err);
+    res.status(500).send('An error has occurred, please bear with us while we look into it');
+});
 
 app.listen(process.env.PORT || 3000);
--- routes/compare.js
+++ routes/compare.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const offer_processor = require('../utils/offer_processor');
 const db = require('../utils/db');
+const _ = require('lodash');
 
 router.post('/', async function (req, res, next) {
     try {
@@ -23,6 +24,28 @@ router.post('/', async function (req, res, next) {
         else {
             res.sendStatus(404);
         }
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+router.post('/single', async function (req, res, next) {
+    try {
+        let offers = db.get('offers');
+
+        let search = {'TDU.Name': req.body.tdu_name};
+
+
+        let filtered_offers = await offers.find(search);
+        let result = offer_processor.process_offers(filtered_offers, req.body.usages);
+        let filtered = _.filter(result.Results, function (item) {
+            return item.Term === 12;
+        });
+        let lowest = _.min(filtered, function (item) {
+            return item.Total;
+        });
+        res.send(lowest);
     }
     catch (err) {
         next(err);
