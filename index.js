ca0c57bec7f2db4964b9b4d7861f497612854c05
Ryan Purcell <patrick.ryan.purcell@gmail.com>
2019-01-02 21:38:14 -0600

    Added ability for cors requests

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger_base');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use(cors());
 
 app.use('/api', require('./routes/api'));
 
